a4e86707eaa000eff92ea27d1b8d8b33a04819b1
William Erthal <83590841+wjerthal@users.noreply.github.com>
2023-03-30 15:07:12 -0300

    Update funcoes.py

--- projetos/calculadora/funcoes.py
+++ projetos/calculadora/funcoes.py
@@ -1,2 +1,7 @@
+
 def soma(a, b):
-    return a + b
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        retorno = f'\nO resultado da soma de {a} com {b} é {a+b}.'
+        return retorno     
+    else:
+        raise TypeError(f"O input 'a' e 'b' devem ser numéricos, recebido a={a} tipo({type(a)}, b={b} tipo({type(b)})")
