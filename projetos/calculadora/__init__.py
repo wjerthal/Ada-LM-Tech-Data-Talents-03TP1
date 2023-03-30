ad01df8a0615004e9226069b251d65a53de824b4
william-erthal <william.erthal@gmail.com>
2023-03-30 14:34:04 -0300

    Formação do menu e função soma inicial

--- projetos/calculadora/__init__.py
+++ projetos/calculadora/__init__.py
@@ -4,3 +4,6 @@ def  calcule():
     a=1
     b=2
     print(soma(a,b))
+
+def  calcule2(a,b,operacao):
+    return operacao(a,b)
--- projetos/calculadora/app.py
+++ projetos/calculadora/app.py
@@ -1,6 +1,38 @@
 import os
 import sys
 sys.path.insert(0,os.getcwd())
-from projetos.calculadora import calcule
 
-calcule()
+from projetos.calculadora import calcule
+from projetos.calculadora import calcule2
+
+from funcoes import soma
+
+while True:
+    print('Caculadora:')
+    print('===========')
+    print('            1) Soma')
+    print('            2) Subtração')
+    print('            3) Multiplicação')
+    print('            4) Divisão')
+    print('            0) Sair')
+    opcao = input('Digite a sua opção: ')
+    if opcao not in ('1', '2', '3', '4', '0'):        
+        continue
+    if opcao == '0':
+        break
+    while True:
+        try:
+            x = float(input('Digite o primeiro número: '))
+        except:
+            print('Número digitado inválido!')
+            continue
+        try:
+            y = float(input('Digite o segundo número: '))
+        except:
+            print('Número digitado inválido!')
+            continue
+        operacao = soma
+        if opcao == '1':
+            calcule2(x,y,operacao)
+        calcule()
+print('Programa finalizado!!')
